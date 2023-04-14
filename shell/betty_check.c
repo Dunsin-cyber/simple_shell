302a6256e93025ddf0c03795ba509dd2e800b21c
Abisuwa <ayodejiabisuwa23@gmail.com>
2023-04-14 23:40:27 +0100

    bug fixed in betty_check.c file

--- shell/betty_check.c
+++ shell/betty_check.c
@@ -7,11 +7,8 @@
 
 int main()
 {
-    char author1;
-    char author2;
-
-    author1 = "Oluwadunsin Abisuwa";
-    author2 = "Benson Odhiambo";
+    char *author1 = "Oluwadunsin Abisuwa";
+    char *author2 = "Benson Odhiambo";
 
     printf("The authors of this project are %s and %s\n", author1, author2);
     return (0);
